d071017d067b89fb8c27d1a9549ff3b8565fe0a0
monicaberrocal <79003682+monicaberrocal@users.noreply.github.com>
2025-02-03 20:20:05 +0100

    start.sh 2

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
-nix-channel --update && \
-nix-env -iA nixpkgs.python3Full && \
-pip install --no-cache-dir --upgrade pip setuptools wheel && \
-pip install --no-cache-dir -r requirements.txt && \
+#!/bin/sh
+
+python3 --version
+which python3
+
+pip install --no-cache-dir --upgrade pip setuptools wheel
+pip install --no-cache-dir -r requirements.txt
+
 celery -A testapp worker --loglevel=info --uid=nobody --gid=nogroup
