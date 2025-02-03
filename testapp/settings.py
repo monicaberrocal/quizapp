60f8e37ac6ffb4079045fe17c9eb93740efcace5
monicaberrocal <79003682+monicaberrocal@users.noreply.github.com>
2025-02-03 13:07:13 +0100

    csrf trust

--- testapp/settings.py
+++ testapp/settings.py
@@ -149,3 +149,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://gemastudies.up.railway.app",  # Agrega tu dominio de Railway aquí
+]
