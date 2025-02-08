3f11ec3b281a274a3154206c9449034a970aa344
monicaberrocal <79003682+monicaberrocal@users.noreply.github.com>
2025-02-08 10:30:36 +0100

    Actualización a Python 3.12 y dependencias

--- testapp/celery.py
+++ testapp/celery.py
@@ -1,8 +1,10 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testapp.settings")
 
-celery_app = Celery("testapp")
-celery_app.config_from_object("django.conf:settings", namespace="CELERY")
-celery_app.autodiscover_tasks()
+app = Celery("testapp")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.autodiscover_tasks()
