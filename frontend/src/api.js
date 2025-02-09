e89e8ee7c23405fc0df2313ca9a19e96dc1bd093
monicaberrocal <79003682+monicaberrocal@users.noreply.github.com>
2025-02-09 18:44:02 +0100

    clean

--- frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_BASE_URL = "https://quizapp-production-6f2c.up.railway.app/api/";
-
-const api = axios.create({
-  baseURL: API_BASE_URL,
-});
-
-export default api;
