ff7be97ba071d41421b431f726ab6ea14e2947aa
monicaberrocal <79003682+monicaberrocal@users.noreply.github.com>
2025-02-09 16:16:38 +0100

    variable

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://quizapp-production-6f2c.up.railway.app/api/";
+const API_BASE_URL = "https://quizapp-production-6f2c.up.railway.app/api/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
