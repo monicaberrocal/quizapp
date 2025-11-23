6bcecca181791405db1f7b68ac92596961c6a9aa
monicaberrocal <79003682+monicaberrocal@users.noreply.github.com>
2025-11-23 10:11:09 +0100

    fix csrf 2

--- backend/testapp/settings.py
+++ backend/testapp/settings.py
@@ -170,7 +170,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS").strip().split(",")
+CSRF_TRUSTED_ORIGINS = [s.strip() for s in config("CSRF_TRUSTED_ORIGINS").split(",")]
 
 
 CELERY_BROKER_URL = config("REDIS_URL")
